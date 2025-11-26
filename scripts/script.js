a3f2c6b38a02c6f7e84dfc6c83c69fa5ad70a36d
kamp <lexiaky@gmail.com>
2025-11-26 17:49:46 -0500

    307sba requirements

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+let buttonCase = document.getElementById("buttonCase");
+let hiddenCase = document.getElementById("hiddenCase");
+
+function showRoutine() {
+    buttonCase.style.display = "none";
+    hiddenCase.style.display = "flex";
+}
+
+function flipback() {
+    if (buttonCase.style.display == "none"
+    && hiddenCase.style.display == "flex"){
+    hiddenCase.style.display = "none"
+    buttonCase.style.display = "flex"}
+}
