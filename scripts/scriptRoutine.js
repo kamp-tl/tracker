defcae498924c0b4e5093d7eadb9a4678b52c357
kamp <lexiaky@gmail.com>
2025-11-29 13:27:16 -0500

    add js to routinne creation

--- scripts/scriptRoutine.js
+++ scripts/scriptRoutine.js
@@ -2,24 +2,67 @@ let buttonPlus = document.getElementById("plus");
 let hiddenTextarea = document.getElementById("hiddenTextarea")
 let textArea = document.getElementById("textArea")
 let textWallEl = document.getElementById("textWall")
+let helpTextEl = document.getElementById("helpText")
+
+let lastEnterTime = 0;
+let doubleEnterThres = 400;
 
 
+
+let activeGroup = null;
 function clickPlus() {
     if (hiddenTextarea.style.display == ""){
         hiddenTextarea.style.display = "flex";
     }
-    else {
-        let newItem = textArea.value;
-        let newLine = document.createElement("p")
-        newLine.textContent = newItem;
-        textWallEl.appendChild(newLine);
-        textArea.value = "";
+    if (activeGroup==null){
+       
+        if(textArea.value.trim() != "") {
+            startGroup()
+            helpTextEl.textContent=("Add Exercise Movement");
+        }
+    }
+    else if (activeGroup!=null){
+       
+        addExercise()
+        helpTextEl.textContent=("Add Exercise Movement");
     }
     textArea.focus();
 }
 
+function startGroup() {
+    let newItem = textArea.value; 
+    let newGroup = document.createElement("div")
+    newGroup.classList.add("moveGroup");
+    newGroup.textContent = newItem;
+    textWallEl.appendChild(newGroup);
+    textArea.value = "";
+    activeGroup = newGroup;
+}
+
+function addExercise() {
+    let exName = textArea.value;
+    let line = document.createElement("div");
+    line.classList.add("exerciseLine");
+    line.textContent = exName;
+    activeGroup.appendChild(line);
+    textArea.value=""
+}
+
 document.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") { 
-      clickPlus();
+    if (event.key === "Enter" ) {
+        const now = Date.now(); 
+      clickPlus()
+      textArea.focus();
+      if (activeGroup!==null && now - lastEnterTime < doubleEnterThres){
+        //double enter 
+        activeGroup = null;
+        helpTextEl.textContent = "Add Movement Group"
+      }
+      else {
+        clickPlus();
+      }
+      lastEnterTime = now;
+      textArea.focus();
+      event.preventDefault();
     }
   });
